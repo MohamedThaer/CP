6d4e343a30589e9baf03b47879b9745d419cb76f
Mohamed Thaer <125297147+MohamedThaer@users.noreply.github.com>
2026-04-14 18:27:32 +0200

    Delete data_structures directory

--- data_structures/ordered_set_erase.cpp
+++
@@ -1,6 +0,0 @@
-template <class M>
-void erase(ordered_set<M> &x, M v) {
-    int rank = x.order_of_key(v);                        // Number of elements that are less than v in t
-    auto it = x.find_by_order(rank); // Iterator that points to the (rank+1)th element in t
-    x.erase(it);
-}
