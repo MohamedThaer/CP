daeffd56489bdcce95c9791e7b49f606171a6d31
Mohamed Thaer <125297147+MohamedThaer@users.noreply.github.com>
2026-07-14 17:05:01 +0300

    Create Hash_(Subtree).cpp

---
+++ Strings/Hash_(Subtree).cpp
@@ -0,0 +1,29 @@
+struct hash_subtree {
+    vector<int> in, out, order;
+    dr_string ds;
+    int timer;
+
+    hash_subtree(int n, int root, const vector<vector<int>>& adj, const vector<int>& vals) {
+        in.resize(n);
+        out.resize(n);
+        order.resize(n);
+        timer = 0;
+
+        function<void(int, int)> dfs = [&](int u, int p) {
+            in[u] = timer;
+            order[timer++] = vals[u];
+            for (int v : adj[u]) {
+                if (v != p) dfs(v, u); // Call dfs directly without passing 'self'
+            }
+            out[u] = timer - 1;
+        };
+        
+        dfs(root, -1);
+        ds = dr_string(order);
+    }
+
+  
+    int64_t query(int u) {
+        return ds.substr(in[u], out[u]);
+    }
+};
