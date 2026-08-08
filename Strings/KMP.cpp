5081a6d4739154960f63e8126dbd8e6952483d82
Mohamed Thaer <125297147+MohamedThaer@users.noreply.github.com>
2026-08-08 23:05:37 +0300

    Update KMP.cpp

--- Strings/KMP.cpp
+++ Strings/KMP.cpp
@@ -27,3 +27,42 @@ vector<int> KMP(const string &str, const string &pat) {
     }
     return ret;
 }
+
+vector<vector<int> > compute_automaton(string s) {
+    s += '#';
+    vector<int> pi = prefix_function(s);
+    int n = s.size();
+    vector<vector<int> > aut(n, vector<int>(26, 0));
+    for (int i = 0; i < n; i++) {
+        for (int c = 0; c < 26; c++) {
+            if (s[i] - 'a' == c)
+                aut[i][c] = i + 1;
+            else if (i)
+                aut[i][c] = aut[pi[i - 1]][c];
+        }
+    }
+    return aut;
+}
+
+vector<pair<int, int>> get_prefix_suffix_occurrences(const string &s) {
+    int n = s.size();
+    vector<int> pi = prefix_function(s);
+    vector<int> freq(n + 1, 0);
+    for (int i = 0; i < n; i++) {
+        freq[pi[i]]++;
+    }
+    for (int i = n - 1; i > 0; i--) {
+        freq[pi[i - 1]] += freq[i];
+    }
+    for (int i = 0; i <= n; i++) {
+        freq[i]++;
+    }
+    vector<pair<int, int>> ans;
+    int len = n;
+    while (len > 0) {
+        ans.push_back({len, freq[len]});
+        len = pi[len - 1];
+    }
+    reverse(ans.begin(), ans.end());
+    return ans;
+}
